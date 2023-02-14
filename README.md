9af78e8e6b4d5dfb980aac0cc8412a36b5d6c234
Amanda Migala <107291640+amigala@users.noreply.github.com>
2023-02-14 05:36:49 +0000

    BeatVersion 1

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 My doddle is about 
 
 #Citations
-- SVG sources
+- SVG sources: https://www.figma.com/file/a55FO3JOrkGDMwMjx1fgDr/DIY-Landing-Page?node-id=0%3A1&t=6StPmMM02fD6gxgA-1
 - Code sources
--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 // Check JS is Connected
 console.log('JS Connected');
+//https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js
+gsap.to('.heart1', {  
+    scale: 1.5,
+    repeat: -1,
+    duration: 3
+  }
+  );
+  gsap.to('.heart2', {  
+    scale: 2,
+    repeat: -1,
+    duration: 3
+  }
+  );
+  gsap.to('.heart3', {  
+    scale: 1.5,
+    repeat: -1,
+    duration: 3
+  }
+  );
